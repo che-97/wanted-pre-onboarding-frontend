77ada0e33e7a27fd9539437d9dc8a9243a9307f5
che-97 <rue015@gmail.com>
2023-02-15 13:48:14 +0900

    refactor : 로그인/회원가입 api 분리

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import {ACCESS_TOKEN} from "../../constants/api-config"
+import { signin, signup } from "../../services/ApiService"
 
 function AuthForm(props) {
   const navigate  = useNavigate();
@@ -38,55 +38,10 @@ function AuthForm(props) {
 
     if (isLogin) {
       //login
-      fetch(
-        "https://pre-onboarding-selection-task.shop/auth/signin",
-        {
-          method: "POST",
-          body: JSON.stringify({email:enteredEmail,password:enteredPassword}),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((response) => {
-        if(response.ok){
-            //todo redirect
-            //save token
-            return response.json()
-        }
-       /* else{
-            //error
-            return response.json().then((data) => {
-                alert(data.message)
-            })
-        }*/
-      }).then((data)=>{
-        localStorage.setItem(ACCESS_TOKEN, data.access_token)
-        navigate("/todo", {replace: true});
-      });
+      signin({email:enteredEmail,password:enteredPassword});
     } else {
       //sign up
-      fetch(
-        "https://pre-onboarding-selection-task.shop/auth/signup",
-        {
-          method: "POST",
-          body: JSON.stringify({email:enteredEmail,password:enteredPassword}),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((response) => {
-        if(response.ok){
-            //login redirect
-            //console.log(response.json())
-            navigate("/signin", {replace: true});
-        }else{
-            //error
-            return response.json().then((data) => {
-                alert(data.message)
-            })
-        }
-        
-      });
+      signup({email:enteredEmail,password:enteredPassword});
     }
   };
 
---
+++ src/services/ApiService.js
@@ -0,0 +1,54 @@
+import { API_BASE_URL, ACCESS_TOKEN } from "../constants/api-config";
+
+export function call(api, method, request) {
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  let options = {
+    headers: headers,
+    url: API_BASE_URL + api,
+    method: method,
+  };
+
+  if (request) {
+    options.body = JSON.stringify(request);
+  }
+  return fetch(options.url, options).then((response) =>
+    response.json().then((json) => {
+      if (!response.ok) {
+        return Promise.reject(json);
+      }
+      return json;
+    })
+  );
+}
+
+export function signin(userDTO) {
+  return call("/auth/signin", "POST", userDTO)
+    .then((response) => {
+      if (response.access_token) {
+        localStorage.setItem(ACCESS_TOKEN, response.access_token);
+        window.location.href = "/todo";
+      }
+    })
+    .catch((error) => {
+        //alert(error.message);
+        alert("email과 password를 확인해주세요.")
+    });
+}
+
+export function signout() {
+  localStorage.setItem(ACCESS_TOKEN, null);
+  window.location.href = "/signin";
+}
+
+export function signup(userDTO) {
+  return call("/auth/signup", "POST", userDTO)
+    .then((response) => {
+      window.location.href = "/signin";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
